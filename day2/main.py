f28e13594901c078ae196e284c5d1078a3a72a09
nekcoj <55090544+nekcoj@users.noreply.github.com>
2022-12-02 19:36:14 +0100

    Day2 Part2

--- day2/main.py
+++ day2/main.py
@@ -1,20 +1,22 @@
 
 matrix = {
-  'AX': 4,
-  'AY': 8,
-  'AZ': 3,
-  'BX': 1,
-  'BY': 5,
-  'BZ': 9,
-  'CX': 7,
-  'CY': 2,
-  'CZ': 6
+  'AX': 0+3, #rock/scissor:1/3'
+  'AY': 3+1, #rock/rock:1/1
+  'AZ': 6+2, #rock/paper:1/2
+  'BX': 0+1, #paper/rock:2/1
+  'BY': 3+2, #paper/paper:2/2
+  'BZ': 6+3, #paper/scissor:2/3
+  'CX': 0+2, #scissor/paper:3/2
+  'CY': 3+3, #scissor/scissor:3/3
+  'CZ': 6+1  #scissor/rock:3/1
 }
-playerScore = 0
+# X lose, Y draw, Z win
 
+playerScore = 0
 def play():
   strategyGuide = None
   input = open('./day2/input', 'r')
+  #strategyGuide = [['A', 'Y'], ['B', 'X'], ['C', 'Z']]
   strategyGuide = input.readlines()
     
   for line in strategyGuide:
