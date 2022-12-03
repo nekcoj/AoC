9e9ef7575a82ea42431bd4f577dcecb06903a9f5
nekcoj <55090544+nekcoj@users.noreply.github.com>
2022-12-03 07:28:57 +0100

    Day3 Part2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Advent of Code 2022.
 
-> - ##### Day 1: [Javascript](https://github.com/nekcoj/AoC/tree/master/day1)
-> - ##### Day 2: [Python](https://github.com/nekcoj/AoC/tree/master/day2)
+> - ##### [Day 1](https://github.com/nekcoj/AoC/tree/master/day1): Javascript
+> - ##### [Day 2](https://github.com/nekcoj/AoC/tree/master/day2): Python
+> - ##### [Day 3](https://github.com/nekcoj/AoC/tree/master/day3): Javascript
--- day3/day3.js
+++ day3/day3.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const readline = require("readline");
 const input = "./day3/input";
-/*
+
 let sampleData = [
   "vJrwpWtwJgWrhcsFMMfFFhFp",
   "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
@@ -10,15 +10,15 @@ let sampleData = [
   "ttgJtRGJQctTZtZT",
   "CrZsJsPPZsGzwwsLwLmpwMDw",
 ];
-*/
+
 // priority a-z 1-26
 // A-Z 27 - 52
 
 let priority = [];
 
 async function getPriority() {
-  /* Loop using sampleData 
-  sampleData.forEach((rucksack) => {
+  // Loop using sampleData
+  /* sampleData.forEach((rucksack) => {
     let items = rucksack.split("");
     console.log(items);
     let compOne = Array.from(new Set(items.slice(0, items.length / 2)));
@@ -33,27 +33,38 @@ async function getPriority() {
       }
     });
   }); */
+
+  // Sample data part2
+  /* sampleData.forEach((rucksack, index, arr) => {
+    if ((index + 1) % 3 === 0) {
+      let items = Array.from(new Set(rucksack.split("")));
+      items.forEach((item) => {
+        if (arr[index - 2].includes(item) && arr[index - 1].includes(item)) {
+          priority.push(item);
+        }
+      });
+    }
+  }); */
+
   // Loop with input from file ()
+  let data = [];
   let r = readline.createInterface({
     input: fs.createReadStream(input),
   });
   r.on("line", function (rucksack) {
-    let items = rucksack.split("");
-    console.log(items);
-    let compOne = Array.from(new Set(items.slice(0, items.length / 2)));
-    let compTwo = Array.from(
-      new Set(items.slice(items.length / 2, items.length))
-    );
-    console.log("Compartment 1:", compOne);
-    console.log("Compartment 2:", compTwo);
-    compOne.forEach((item) => {
-      if (compTwo.includes(item)) {
-        priority.push(item);
-      }
-    });
+    data.push(rucksack);
   });
   await new Promise((res) => r.once("close", res));
-
+  data.forEach((rucksack, index, arr) => {
+    if ((index + 1) % 3 === 0) {
+      let items = Array.from(new Set(rucksack.split("")));
+      items.forEach((item) => {
+        if (arr[index - 2].includes(item) && arr[index - 1].includes(item)) {
+          priority.push(item);
+        }
+      });
+    }
+  });
   console.log(priority);
   let prioritySum = 0;
   priority.forEach((itemType) => {
