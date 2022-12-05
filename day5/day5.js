83d30b8c4827178fe13ccc7bdc96cb354270e2ee
nekcoj <55090544+nekcoj@users.noreply.github.com>
2022-12-05 08:16:52 +0100

    Day 5 part 1

---
+++ day5/day5.js
@@ -0,0 +1,55 @@
+const fileReader = require("../utils/fileReader.js");
+
+let data = null;
+
+async function readData() {
+  data = await fileReader("./day5/input");
+}
+let sampleStacks = {
+  1: ["Z", "N"],
+  2: ["M", "C", "D"],
+  3: ["P"],
+};
+
+let stacks = {
+  1: ["Z", "T", "F", "R", "W", "J", "G"],
+  2: ["G", "W", "M"],
+  3: ["J", "N", "H", "G"],
+  4: ["J", "R", "C", "N", "W"],
+  5: ["W", "F", "S", "B", "G", "Q", "V", "M"],
+  6: ["S", "R", "T", "D", "V", "W", "C"],
+  7: ["H", "B", "N", "C", "D", "Z", "G", "V"],
+  8: ["S", "J", "N", "M", "G", "C"],
+  9: ["G", "P", "N", "W", "C", "J", "D", "L"],
+};
+
+async function start() {
+  await readData();
+  moveCrates();
+}
+
+function moveCrates() {
+  let moves = [];
+  data.forEach((line) => {
+    moves.push(
+      line
+        .replace(
+          /(\w+ )(?<first>\d+)( \w+ )(?<second>\d+)( \w+ )(?<third>\d+)/gm,
+          "$<first> $<second> $<third>"
+        )
+        .split(" ")
+    );
+  });
+  moves.forEach((move) => {
+    for (let i = 0; i < move[0]; i++) {
+      stacks[move[2]].push(stacks[move[1]][stacks[move[1]].length - 1]);
+      stacks[move[1]].length = stacks[move[1]].length - 1;
+    }
+  });
+  let topStack = [];
+  Object.values(stacks).forEach((stack) => {
+    topStack.push(stack[stack.length - 1]);
+  });
+  console.log(topStack.join(""));
+}
+start();
