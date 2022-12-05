edc6e6cd0f3150d4ae601730b22d9d9e4d4ece04
nekcoj <55090544+nekcoj@users.noreply.github.com>
2022-12-05 08:36:41 +0100

    Day5 Part2

--- day5/day5.js
+++ day5/day5.js
@@ -25,10 +25,10 @@ let stacks = {
 
 async function start() {
   await readData();
-  moveCrates();
+  moveCratesPart2();
 }
 
-function moveCrates() {
+function moveCratesPart1() {
   let moves = [];
   data.forEach((line) => {
     moves.push(
@@ -46,6 +46,40 @@ function moveCrates() {
       stacks[move[1]].length = stacks[move[1]].length - 1;
     }
   });
+  let topStack = [];
+  Object.values(stacks).forEach((stack) => {
+    topStack.push(stack[stack.length - 1]);
+  });
+  console.log(topStack.join(""));
+}
+
+function moveCratesPart2() {
+  let moves = [];
+  data.forEach((line) => {
+    moves.push(
+      line
+        .replace(
+          /(\w+ )(?<first>\d+)( \w+ )(?<second>\d+)( \w+ )(?<third>\d+)/gm,
+          "$<first> $<second> $<third>"
+        )
+        .split(" ")
+    );
+  });
+  moves.forEach((move) => {
+    let bla = stacks[move[1]].slice(
+      stacks[move[1]].length - move[0],
+      stacks[move[1].length - 1]
+    );
+    console.log(bla);
+    stacks[move[2]].push(
+      ...stacks[move[1]].slice(
+        stacks[move[1]].length - move[0],
+        stacks[move[1].length - 1]
+      )
+    );
+    stacks[move[1]].length -= move[0];
+  });
+  console.log(stacks);
   let topStack = [];
   Object.values(stacks).forEach((stack) => {
     topStack.push(stack[stack.length - 1]);
