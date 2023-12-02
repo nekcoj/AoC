642f46479c3ca6ebbcf0a0e0b5e03b309f01e762
Joacim Norbeck <joacimnorbeck@Lenovoideapad33015IKB85a54a59.mynet>
2023-12-02 16:04:40 +0100

    Updated for part 2

--- 2023/day1/main.js
+++ 2023/day1/main.js
@@ -1,40 +1,51 @@
 const { readData } = require('../../utils/fileReader');
 
 let data = null;
-let regex = /([0-9])/g;
+let regex = /(?=(([0-9])|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)))/gm;
+let valueMap = {
+	'one': 1,
+	'two': 2,
+	'three': 3,
+	'four': 4,
+	'five': 5,
+	'six': 6,
+	'seven': 7,
+	'eight': 8,
+	'nine': 9
+}
 
 async function start() {
 	data = await readData(2023, 1);
 	// console.log(data);
-	partA();
+	let values = getValues();
+	console.log(values);
+	let sum = reducer(values);
+	console.log(sum);
 }
 
 start();
 
-function partA() {
-	let values = getValues();
-	//console.log(values);
-	let sum = reducer(values);
-	//console.log(sum);
-}
-
-
 function getValues() {
 	let arr = [];
 	data.forEach(line => {
-		let values = [...line.matchAll(regex)];
-		arr.push(`${values[0][0]}${values[values.length - 1][0]}`);
+		let values = line.matchAll(regex);
+		let temp = [];
+		for (const val of values) {
+			temp.push(val[1]);
+		}
+		arr.push(`${convertValue(temp[0])}${convertValue(temp[temp.length - 1])}`);
 	});
 	return arr;
 }
 
 function reducer(list) {
-	return list.reduce((prev, curr) => convertValue(prev) + convertValue(curr), 0);
+	return list.reduce((prev, curr) => parseInt(prev) + parseInt(curr), 0);
 }
 
 function convertValue(value) {
 	let parsed = parseInt(value);
-	if (parsed != NaN) {
+	if (!isNaN(parsed)) {
 		return parsed;
 	}
+	return valueMap[value];
 }
