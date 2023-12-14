7d4bc53a15ac5ef4df8220fd635f80fe1852ee71
Joacim Norbeck <55090544+nekcoj@users.noreply.github.com>
2023-12-14 13:45:14 +0100

    Day 3 - part 2

--- 2023/day3/main.js
+++ 2023/day3/main.js
@@ -6,18 +6,25 @@ let validPositions = [
 ];
 let matches = [];
 let partNumbers = [];
+let part2numbers = [];
 
 async function start() {
 	data = await readData(2023, 3);
 	parseData();
 	checkMatches();
-	console.log(partNumbers.reduce((acc, cur) => acc + cur, 0));
+	// console.log(partNumbers.reduce((acc, cur) => acc + cur, 0));
+	console.log(part2numbers);
+	console.log(part2numbers.reduce((acc, cur) => acc + cur, 0));
+
 }
 
 function parseData() {
 	for (let i = 0; i < data.length - 1; i++) {
 		for (let j = 0; j < data[i].length; j++) {
-			if(!valueArr.includes(data[i][j])) {
+			// if(!valueArr.includes(data[i][j])) {
+
+			// adjusted for part 2
+			if (['*'].includes(data[i][j])) {
 				matches.push([data[i-1].slice(j - 3, j + 4), data[i].slice(j - 3, j + 4), data[i+1].slice(j - 3, j + 4)]);
 			}
 		}
@@ -28,9 +35,14 @@ function checkMatches() {
 	// console.log(matches);
 	
 	matches.forEach(match => {
-		match.forEach(row => {
-			checkRow(row)
-		});
+		// updated for part 2
+		let p2 = [...checkRow(match[0]), ...checkRow(match[1]), ...checkRow(match[2])];
+		if (p2.length > 1) {
+			part2numbers.push(p2.reduce((acc, cur) => acc * cur, 1));
+		}
+		// match.forEach(row => {
+		// 	checkRow(row)
+		// });
 	});
 }
 
@@ -53,19 +65,19 @@ function checkRow(row) {
 	}
 	let positions = val.pos.join('').split('.');
 	let numbers = val.num.join('').split('.');
+	let part2 = [];
 	positions.forEach(pos => {
 		if (validPositions.includes(pos)) {
-		 	partNumbers.push(parseInt(numbers[positions.indexOf(pos)]));
-		}
-	})
-}
+		 	// partNumbers.push(parseInt(numbers[positions.indexOf(pos)]));
 
-function sum() {
-	let sum = 0;
-	partNumbers.forEach(num => {
-		sum += num;
+			// added for part 2
+			console.log('part2 before add', part2);
+			part2.push(parseInt(numbers[positions.indexOf(pos)]));
+			console.log('part2 after add', part2);
+		}
 	});
-	return sum;
+	// updated for part2
+	return part2;
 }
 
 start();
